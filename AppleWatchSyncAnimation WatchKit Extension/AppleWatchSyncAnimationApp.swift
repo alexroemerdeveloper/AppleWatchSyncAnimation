02af802c1e7464ad03a451f82ec84541dcd388df
Alexander Römer <alexroemerdeveloper@gmail.com>
2021-04-03 20:03:51 +0200

    Initial Commit

---
+++ AppleWatchSyncAnimation WatchKit Extension/AppleWatchSyncAnimationApp.swift
@@ -0,0 +1,19 @@
+//
+//  AppleWatchSyncAnimationApp.swift
+//  AppleWatchSyncAnimation WatchKit Extension
+//
+//  Created by Alexander Römer on 03.04.21.
+//
+
+import SwiftUI
+
+@main
+struct AppleWatchSyncAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
---
+++ AppleWatchSyncAnimation WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleWatchSyncAnimation WatchKit Extension
+//
+//  Created by Alexander Römer on 03.04.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
